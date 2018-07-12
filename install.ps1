7dc02477f618328083e7a29162bb4aa97821c97d
Maciej Janiszewski <chleb@krojony.pl>
2018-07-12 11:57:52 +0200

    force LF in Git on Windows

--- install.ps1
+++ install.ps1
@@ -2,14 +2,14 @@
 $scripts_path = Split-Path -Path $profile
 
 if (Test-Path $scripts_path -PathType Container) {
-    echo "POSH config directory exists, remove it first"
-    rm -Force $scripts_path
+    Write-Output "POSH config directory exists, remove it first"
+    Remove-Item -Force $scripts_path
 }
 New-Item -Type SymbolicLink -Path $scripts_path -Value $(Join-Path $PSScriptRoot posh)
 
 # install plug
-cp -r .vim ~
-md ~/.vim/autoload
+Copy-Item -r .vim ~
+mkdir ~/.vim/autoload
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile(
   $uri,
@@ -21,6 +21,7 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # symlinks
 New-Item -Type SymbolicLink -Path ~/.vimrc -Value $(Join-Path $PSScriptRoot .vimrc)
 New-Item -Type SymbolicLink -Path ~/.gitconfig -Value $(Join-Path $PSScriptRoot .gitconfig_win)
+New-Item -Type SymbolicLink -Path ~/.gitattributes -Value $(Join-Path $PSScriptRoot .gitattributes)
 
 # posh modules
 Install-Module posh-git
